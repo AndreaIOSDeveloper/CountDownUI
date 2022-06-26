30d78c51c0a047a1d166bd4440795ddef2b083aa
Di Francia <adifra291@resml-1721038.home-life.hub>
2022-06-26 13:20:19 +0200

    improve tags view

--- CountDown/AddNewTimer.swift
+++ CountDown/AddNewTimer.swift
@@ -1,8 +1,25 @@
 //
-//  AddNewTImer.swift
+//  AddNewTimer.swift
 //  CountDown
 //
 //  Created by Di Francia, Andrea (Contractor) on 26/06/22.
 //
 
 import Foundation
+import SwiftUI
+
+struct AddNewTimer: View {
+    @Binding var presentedAsModal: Bool
+
+    @StateObject private var viewModel = DataViewModel.shared
+    
+    var body: some View {
+        Button("dismiss") { self.presentedAsModal = false }
+    }
+}
+
+struct AddNewTimer_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewTimer(presentedAsModal: .constant(true))
+    }
+}
