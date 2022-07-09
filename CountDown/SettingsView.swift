e34dd361cf96fd141aea21bfc558af2a5c992695
Di Francia <adifra291@resml-1721038.home-life.hub>
2022-07-10 00:52:25 +0200

    add profile view

--- CountDown/SettingsView.swift
+++ CountDown/SettingsView.swift
@@ -14,13 +14,112 @@ struct SettingsView: View {
     
     @State private var isTimerRunning = false
     @State private var startTime: Date?
-    @State private var timerString = "00:00:00"
+    @State private var dateComponents: DateComponents?
+    @State private var isToggle : Bool = false
+
+    let settings = [
+           "Notification",
+           "Privacy",
+           "Contact us",
+           "Credits",
+       ]
     
-    init() { }
+    init() {   }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Time in community:")
+                    .padding()
+                    .multilineTextAlignment(.leading)
+                    .font(.body)
+                    .foregroundColor(.gray)
+            HStack {
+                VStack {
+                    Text(String(format: "%02d", dateComponents?.day ?? 00))
+                        .bold()
+                        .foregroundColor(Color.primary)
+                    Text("Days")
+                        .foregroundColor(Color.primary)
+                }
+                .padding(.trailing)
+                
+                VStack {
+                    Text(String(format: "%02d", dateComponents?.hour ?? 00))
+                        .bold()
+                        .foregroundColor(Color.primary)
+                    Text("Hours")
+                        .foregroundColor(Color.primary)
+                }
+                .padding(.trailing)
+                
+                VStack {
+                    Text(String(format: "%02d", dateComponents?.minute ?? 00))
+                        .bold()
+                        .foregroundColor(Color.primary)
+                    Text("Mins")
+                        .foregroundColor(Color.primary)
+                }
+                .padding(.trailing)
+                
+                VStack {
+                    Text(String(format: "%02d", dateComponents?.second ?? 00))
+                        .bold()
+                        .foregroundColor(Color.primary)
+                    Text("Secs")
+                        .foregroundColor(Color.primary)
+                }
+                
+            }
+            .onReceive(timer) { _ in
+                if self.isTimerRunning {
+                    dateComponents = Date().passedTime(from: startTime ?? Date())
+                }
+            }
+            .onAppear() {
+                if !isTimerRunning {
+                    receiceCurrentDateAppUsed { date in
+                        if date == nil {
+                            saveCurrentUserDate()
+                            startTime = Date()
+                        } else {
+                            startTime = date ?? Date()
+                        }
+                    }
+                }
+                isTimerRunning.toggle()
+            }
+                
+                Spacer()
+             
+                List(settings, id: \.self) { setting in
+                    if setting == "Notification" {
+                        Toggle(setting, isOn: $isToggle)
+                    } else {
+                    
+                    NavigationLink(destination: PlayerView(name: setting)) {
+                        Text(setting)
+                    }
+                    }
+                }
+                .navigationTitle("Profile")
+                
+                Text("Made with ❤️ from Andrea Di Francia")
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+            }
+            
+            
+        }
+        .navigationTitle("Profile")
+        .navigationBarTitleDisplayMode(.large)
+    }
     
     private func saveCurrentUserDate() {
         let userDefaults = UserDefaults.standard
-
+        
         do {
             try userDefaults.setObject(Date(), forKey: "UserDateStartApp")
             print("🟢 Salvo Data Iniziale di utilizzo l'app: \(Date())")
@@ -41,33 +140,6 @@ struct SettingsView: View {
             callback(nil)
         }
     }
-    
-    var body: some View {
-        NavigationView {
-            Text(self.timerString)
-                        .font(Font.system(.largeTitle, design: .monospaced))
-                        .onReceive(timer) { _ in
-                            if self.isTimerRunning {
-                                timerString = Date().passedTime(from: startTime ?? Date())
-                            }
-                        }
-                        .onAppear() {
-                            if !isTimerRunning {
-                                timerString = "00:00:00"
-                                receiceCurrentDateAppUsed { date in
-                                    if date == nil {
-                                        saveCurrentUserDate()
-                                    } else {
-                                        startTime = date ?? Date()
-                                    }
-                                }
-                            }
-                            isTimerRunning.toggle()
-                        }
-        }
-        .navigationTitle("Profile")
-        .navigationBarTitleDisplayMode(.large)
-    }
 }
 
 struct SettingsView_Previews: PreviewProvider {
@@ -77,12 +149,22 @@ struct SettingsView_Previews: PreviewProvider {
 }
 
 extension Date {
-    func passedTime(from date: Date) -> String {
-        let difference = Calendar.current.dateComponents([.minute, .second], from: date, to: self)
-        let strOre = String(format: "%02d", difference.hour ?? 00)
-        let strMin = String(format: "%02d", difference.minute ?? 00)
-        let strSec = String(format: "%02d", difference.second ?? 00)
-        
-        return "\(strOre):\(strMin):\(strSec)"
+    func passedTime(from date: Date) -> DateComponents {
+        let difference = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: date, to: self)
+//        let strDay = String(format: "%02d", difference.day ?? 00)
+//        let strOre = String(format: "%02d", difference.hour ?? 00)
+//        let strMin = String(format: "%02d", difference.minute ?? 00)
+//        let strSec = String(format: "%02d", difference.second ?? 00)
+        return difference
+//        return "day: \(strDay) Hours:\(strOre):\(strMin):\(strSec)"
+    }
+}
+
+struct PlayerView: View {
+    let name: String
+
+    var body: some View {
+        Text("Selected player: \(name)")
+            .font(.largeTitle)
     }
 }
